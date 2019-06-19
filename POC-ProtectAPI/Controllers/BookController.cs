dd69fcffc9cbe36459dce18687c84e02063a65fe
Carlos Andrés <51211254+carlosandrescorcoran@users.noreply.github.com>
2019-06-19 09:29:11 -0500

    First commit

---
+++ POC-ProtectAPI/Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using POC_ProtectAPI.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POC_ProtectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        [HttpGet("getBooks")]
+        public IActionResult GetBooks()
+        {
+            try
+            {
+                return Ok("");
+            }
+            catch (Exception)
+            {
+                return BadRequest("");
+            }
+        }
+
+        [FeatureToggleValidation(ToggleName = "add-book")]
+        [HttpPost("addBook")]
+        public IActionResult AddBook()
+        {
+            try
+            {
+                return Ok("");
+            }
+            catch (Exception)
+            {
+                return BadRequest("");
+            }
+        }
+
+    }
+}
---
+++ POC-ProtectAPI/Helpers/FeatureToggleValidationAttribute.cs
@@ -0,0 +1,36 @@
+﻿using GraphQL.Client;
+using GraphQL.Common.Request;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace POC_ProtectAPI.Helpers
+{
+    public class FeatureToggleValidationAttribute : ActionFilterAttribute
+    {
+        public string ToggleName { get; set; }
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var bookRequest = new GraphQLRequest
+            {
+                Query = "{ flag(feature: " + ((char)34) + ToggleName + ((char)34) + "){ feature enabled } }",
+                Variables = new
+                {
+                    featurep = ToggleName
+                }
+            };
+            var graphQLClient = new GraphQLClient("http://localhost:4000/graphql");
+            var graphQLResponseTask = graphQLClient.PostAsync(bookRequest);
+            graphQLResponseTask.Wait();
+            var featureEnabled = graphQLResponseTask.Result.Data.flag.enabled.Value;
+            if (!featureEnabled)
+            {
+                context.Result = new BadRequestObjectResult("User unauthorized.");
+            }   
+        }
+    }
+}
